025044ea247b5e82b7ddadb059d240167c2c8492
Akbar <akbarali.dev1@gmail.com>
2022-07-03 19:41:03 +0500

    Sheharzad commit

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/style-prop-object */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
@@ -6,11 +7,26 @@ import './ContactUs.css';
 const ContactUs = () => {
     return (
         <Container className="ContactUs">
-            <div class="ContactUs">
+            <div className="ContactUs">
                 <h1>Contact Us</h1>
+
+                <div class="Contact">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <div className="ContactTitle">
+                                <h2>Sheharzad Salahuddin</h2>
+                            </div>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="ContactTitle">
+                                <span>Email: <p><a href="mailto:sheharzad.salahuddin9000@outlook.com">sheharzad.salahuddin9000@outlook.com</a></p></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </Container>
-        
+
     )
 }
 
